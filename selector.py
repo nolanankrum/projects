a8e750514fdd2424637120198d1b9cab8f4bc3d8
nolan <nolanankrum@gmail.com>
2022-07-15 14:55:25 -0500

    commited

---
+++ selector.py
@@ -0,0 +1,13 @@
+import random
+
+friends = [
+    'Melody',
+    'Mc',
+    'Alec',
+    'Bryce',
+    'Jon'
+]
+selected = random.choice(friends) 
+
+print('Who should I facetime today?')
+print(selected)
